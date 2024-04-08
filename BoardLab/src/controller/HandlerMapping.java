01e7951cac113973cd55e32b643aaaab8f85f51c
ashram7 <ashram7@hanmail.net>
2024-04-08 16:34:10 +0900

    second commit

--- BoardLab/src/controller/HandlerMapping.java
+++ BoardLab/src/controller/HandlerMapping.java
@@ -3,12 +3,14 @@ package controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import controller.user.InsertUserController;
+
 public class HandlerMapping {
 	private Map<String, Controller> mappings;
 	
 	public HandlerMapping() {
 		mappings = new HashMap<String, Controller>();
-		
+		mappings.put("/insertUser.do", new InsertUserController());
 		
 	}
 	
